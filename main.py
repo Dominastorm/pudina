523d3a4368b05f64b0113f67004e24f8b8c26015
Dhruv Chawla <dchawla228@gmail.com>
2022-11-15 17:09:44 +0530

    Add file download

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import io
 st.set_page_config(layout="wide")
 
 # Add a logo to the sidebar
-image = Image.open('assets/images/Pudina.png')
+image = Image.open('assets/images/pudina.png')
 # Small logo for sidebar
 st.sidebar.image(image, use_column_width=True)
 
@@ -27,4 +27,31 @@ add_selectbox = st.sidebar.selectbox(
     "How would you like to view the data?",
     ("Dashboard", "Data")
 )
+
+
+# DASHBOARD
+#Add an app title. Use css to style the title
+st.markdown(""" <style> .font {                                          
+    font-size:30px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    </style> """, unsafe_allow_html=True)
+st.markdown('<p class="font">Upload your project plan file and generate Gantt chart instantly</p>', unsafe_allow_html=True)
+
+#Add a template screenshot as an example 
+st.subheader('Step 1: Download the project plan template')
+image = Image.open('assets/images/example.png') #template screenshot provided as an example
+st.image(image,  caption='Make sure you use the same column names as in the template')
+
+#Allow users to download the template
+@st.cache
+def convert_df(df):
+     return df.to_csv().encode('utf-8')
+df=pd.read_csv('assets/documents/template.csv')
+csv = convert_df(df)
+st.download_button(
+     label="Download Template",
+     data=csv,
+     file_name='project_template.csv',
+     mime='text/csv',
+ )
+
 
